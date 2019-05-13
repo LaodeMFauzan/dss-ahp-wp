d611d82de7e23370ca77de5a2b0e5a858d834c88
LaodeMFauzan <fauzanlaode@yahoo.co.id>
2019-05-13 13:43:41 +0700

    added : switch screen from upload to result

--- src/filkom/skripsi/controller/HomeController.java
+++ src/filkom/skripsi/controller/HomeController.java
@@ -5,6 +5,9 @@ import filkom.skripsi.model.FileProperties;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
@@ -12,13 +15,16 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.regex.Pattern;
 
@@ -47,9 +53,13 @@ public class HomeController {
     private int numOfCriteria;
 
     @FXML
-    public void handleClick(MouseEvent mouseEvent) {
+    public void handleClick(MouseEvent mouseEvent) throws IOException {
         if (mouseEvent.getSource() == btn_compute) {
             computeAHP_WP();
+            ScreenController screenController = new ScreenController(ap_root.getScene());
+            URL url = new File("src/filkom/skripsi/gui/result_layout.fxml").toURL();
+            screenController.addScreen("Result", FXMLLoader.load(url));
+            screenController.activate("Result");
         } else if (mouseEvent.getSource() == btn_open_file) {
             openFile();
         }
--- src/filkom/skripsi/controller/ResultController.java
+++ src/filkom/skripsi/controller/ResultController.java
@@ -1,4 +1,9 @@
 package filkom.skripsi.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
 public class ResultController {
+
+
 }
